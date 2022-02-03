86507bad14808e0e7b516bd1bba463cf761b9ab7
Shafiul Haque <shaque30@stuy.edu>
2022-02-03 10:42:55 -0500

    finishedprintNoDoubleLetterWords function

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -19,4 +19,41 @@ public class Recursion{
              }
            }
          }
+
+         /*Print all words that are made of the characters in the array of letters.
+          *There may not be consecutive equal letters, so:
+          *aax is not allowed, but axa is allowed.
+          *@param length : the length of the words that are to be printed
+          *@param letters: the letters you should be using,
+          *@precondition: letters contains at least 2 characters, and has no duplicates.
+          */
+          public static void printNoDoubleLetterWords(int length,char[] letters){
+            printNoDoubleLetterWords(length,"",letters);
+          }
+
+          /*Print all words that are made of the characters in letters. There may not be consecutive equal letters,
+          *aax is not allowed, but axa is allowed.
+          *@param length : either how many more letters need to be
+          *@param word   : the partial word so far.
+          *@param letters: the letters you should be using
+          */
+          public static void printNoDoubleLetterWords(int length,String word,char[]letters){
+            if (length == 0){
+              boolean joe = true;
+              for (int i = 0; i < word.length()-1; i++){
+                if (word.charAt(i) == word.charAt(i+1)) joe = false;
+              }
+              if (joe) System.out.println(word);
+            } else {
+              for (int i = 0; i < letters.length; i++){
+                printNoDoubleLetterWords(length-1, word+letters[i], letters);
+              }
+            }
+          }
+
+         public static void main (String args[]){
+           printAllWords(3);
+           char[] joe = {'a','b','c'};
+           printNoDoubleLetterWords(4, joe);
+         }
 }
