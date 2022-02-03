905475123c0636d733fc668a46ed6c97a2946a0e
Shafiul Haque <shaque30@stuy.edu>
2022-02-03 13:03:30 -0500

    made some changes, almost done

--- Classwork/toWords.java
+++ Classwork/toWords.java
@@ -1,8 +1,25 @@
 public class toWords{
   public static String toWords(int n){
-    return "";
+    String[] ones = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen"};
+    String[] tens = {"teen", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+    if(n<=13){
+      return ones[n];
+    }
+    if (n > 13 && n < 100){
+      int ten = n/10;
+      if (n%10 == 0) return tens[ten-1];
+      if (ten-1 == 0) return toWords(n%10) + tens[ten-1];
+      return tens[ten-1] + " " + toWords(n%10);
+    }
+    if (n > 100 && n < 1000){
+      int hun = n/100;
+      return ones[hun] + " hundred and " + toWords(n%100);
+    }
+    return "didn't work";
   }
   public static void main (String args[]){
-    
+    for (int i = 0; i <= 100; i++){
+      System.out.println(toWords(i));
+    }
   }
 }
