d25d6ad1a626cc8a37de0e4f9675c8833c381ad8
Shafiul Haque <shaque30@stuy.edu>
2022-02-04 08:37:27 -0500

    finished code

--- Classwork/toWords.java
+++ Classwork/toWords.java
@@ -1,25 +1,35 @@
 public class toWords{
   public static String toWords(int n){
-    String[] ones = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen"};
+    String[] ones = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+    String[] teens = {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"};
     String[] tens = {"teen", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
-    if(n<=13){
+    if(n<=10){
       return ones[n];
     }
-    if (n > 13 && n < 100){
+    if(n>10 && n <= 19){
+      return teens[n-11];
+    }
+    if (n >= 20 && n < 100){
       int ten = n/10;
       if (n%10 == 0) return tens[ten-1];
-      if (ten-1 == 0) return toWords(n%10) + tens[ten-1];
-      return tens[ten-1] + " " + toWords(n%10);
+      return tens[ten-1] + "-" + toWords(n%10);
     }
-    if (n > 100 && n < 1000){
+    if (n >= 100 && n < 1000){
       int hun = n/100;
+      if (n%100 == 0) return ones[hun] + " hundred";
       return ones[hun] + " hundred and " + toWords(n%100);
+    }
+    if (n >= 1000 && n < 10000){
+      int thou = n/1000;
+      if (n%1000 == 0) return ones[thou] + " thousand";
+      return ones[thou] + " thousand " + toWords(n%1000);
     }
     return "didn't work";
   }
   public static void main (String args[]){
-    for (int i = 0; i <= 100; i++){
+    for (int i = 0; i <= 2000; i++){
       System.out.println(toWords(i));
     }
+    System.out.println(toWords(342));
   }
 }
