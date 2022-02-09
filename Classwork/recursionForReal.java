541cb1790637e815939682791005f92b0f410fb5
Shafiul Haque <shaque30@stuy.edu>
2022-02-09 10:46:39 -0500

    RecursionClasswork finished groupsum6 and 5

--- Classwork/recursionForReal.java
+++ Classwork/recursionForReal.java
@@ -9,5 +9,15 @@ public class recursionForReal{
           }
     return joe;
   }
+  public static boolean groupSum5(int start, int[] nums, int target) {
+  if (target == 0 && start == nums.length) return true;
+          if (target < 0) return false;
+          boolean joe = false;
+          if (start < nums.length){
+            if(start == 0 || nums[start-1]%5 != 0 || nums[start] != 1) joe = joe || groupSum5(start+1, nums, target-nums[start]);
+            if (nums[start]%5 != 0) joe = joe || groupSum5(start+1, nums, target);
+        }
+  return joe;
+}
 
 }
