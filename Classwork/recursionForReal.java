937ec64e8a675bfa63e52f0a4ee917fa70fac1d0
Shafiul Haque <shaque30@stuy.edu>
2022-02-09 10:38:13 -0500

    RecursionClasswork making progress on groupsum6

--- Classwork/recursionForReal.java
+++ Classwork/recursionForReal.java
@@ -1,3 +1,15 @@
 public class recursionForReal{
+  public static boolean groupSum6(int start, int[] nums, int target) {
+    if (target == 0 && start == nums.length-1) return true;
+            if (target < 0) return false;
+            boolean joe = false;
+            if (start < nums.length){
+            for (int i = start; i < nums.length; i++){
+              joe = joe || groupSum6(start+1, nums, target-nums[start]);
+              if (nums[start] != 6) joe = joe || groupSum6(start+1, nums, target);
+            }
+          }
+    return joe;
+  }
 
 }
