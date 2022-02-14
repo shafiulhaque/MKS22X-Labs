2c639add180708faa9e23e34544329eac533d981
Shafiul Haque <shaque30@stuy.edu>
2022-02-14 10:26:43 -0500

    finished constructor

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -1,6 +1,8 @@
 public class QueenBoard{
   private int[][]board;
-  public QueenBoard(int size){}{}
+  public QueenBoard(int size){
+    board = new int[size][size];
+  }
   private boolean addQueen(int r, int c)
   private void removeQueen(int r, int c)
   /**
