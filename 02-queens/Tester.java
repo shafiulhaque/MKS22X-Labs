3b11bbf4a5478d75d956c1e790e537c59d813198
Shafiul Haque <shaque30@stuy.edu>
2022-02-16 10:38:47 -0500

    modified tester

--- 02-queens/Tester.java
+++ 02-queens/Tester.java
@@ -2,11 +2,13 @@ public class Tester{
   public static void main (String args[]){
     // QueenBoard joe = new QueenBoard(8);
     // System.out.println(joe);
-    // System.out.println(Text.CLEAR_SCREEN);
-    // System.out.println(Text.HIDE_CURSOR);
-    // System.out.println(Text.go(1,1));
-    // joe.solve();
-    // System.out.println(Text.RESET);
+    // // System.out.println(Text.CLEAR_SCREEN);
+    // // System.out.println(Text.HIDE_CURSOR);
+    // // System.out.println(Text.go(1,1));
+    // System.out.println(joe.countSolutions());
+    // // System.out.println(Text.RESET);
+    // System.out.println(joe);
+
     int SIZE = 8;
           if(args.length > 0){
             SIZE = Integer.parseInt(args[0]);
