cbd6632a0fb591262e4b21ae3fb26ac593c8d205
Shafiul Haque <shaque30@stuy.edu>
2022-02-16 10:26:15 -0500

    added mr.k tester

--- 02-queens/Tester.java
+++ 02-queens/Tester.java
@@ -1,13 +1,27 @@
 public class Tester{
   public static void main (String args[]){
-    QueenBoard joe = new QueenBoard(8);
-    System.out.println(joe);
-    System.out.println(Text.CLEAR_SCREEN);
-    System.out.println(Text.HIDE_CURSOR);
-    System.out.println(Text.go(1,1));
-    joe.solve();
-    System.out.println(Text.RESET);
-    System.out.println(joe);
-    System.out.println(joe.countSolutions());
+    // QueenBoard joe = new QueenBoard(8);
+    // System.out.println(joe);
+    // System.out.println(Text.CLEAR_SCREEN);
+    // System.out.println(Text.HIDE_CURSOR);
+    // System.out.println(Text.go(1,1));
+    // joe.solve();
+    // System.out.println(Text.RESET);
+    int SIZE = 8;
+          if(args.length > 0){
+            SIZE = Integer.parseInt(args[0]);
+          }
+          QueenBoard b = new QueenBoard(SIZE);
+          if(args.length > 1){
+            b.setAnimate(true);
+            b.setDelay(Integer.parseInt(args[1]));
+          }
+          System.out.println(Text.CLEAR_SCREEN);
+          System.out.println(Text.HIDE_CURSOR);
+          System.out.println(Text.go(1,1));
+          b.solve();
+          System.out.println(Text.RESET);
+          System.out.println(Text.go(1,1));
+          System.out.println(b);
   }
 }
