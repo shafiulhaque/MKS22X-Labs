7618799d197d5242af39d43ec99cbf7e6415f1b3
Shafiul Haque <shaque30@stuy.edu>
2022-02-17 10:57:39 -0500

    added more features for the text

--- 02-queens/Text.java
+++ 02-queens/Text.java
@@ -18,4 +18,54 @@ public class Text{
               catch (InterruptedException e) {
               }
             }
+
+            public static final int BLACK = 30;
+            public static final int RED = 31;
+            public static final int GREEN = 32;
+            public static final int YELLOW = 33;
+            public static final int BLUE = 34;
+            public static final int MAGENTA = 35;
+            public static final int CYAN = 36;
+            public static final int WHITE = 37;
+
+            /*Text modifiers to be ADDED to a color*/
+            public static final int BACKGROUND = 10;
+            public static final int BRIGHT = 60;
+
+            /*Text modifiers that are separate from color*/
+            public static final int BOLD = 1;
+            public static final int UNDERLINE = 4;
+            public static final int INVERTED = 7;
+
+            /*Reset colors*/
+            public static void reset(){
+              System.out.print("\u001b[0m");
+            }
+
+
+            public static void hideCursor(){
+              System.out.print("\u001b[?25l");
+            }
+
+            public static void showCursor(){
+              System.out.print("\u001b[?25h");
+            }
+
+            /*Erases all text on the terminal.*/
+            public static void clear(){
+              System.out.print("\u001b[2J");
+            }
+
+            /*Overloaded Colorize methods.
+            c1,c2 and c3 are any color modifiers such as bold/color/background color etc.
+            */
+            public static String colorize(String text,int c1){
+              return ("\u001b[" + c1 + "m"+text+"\u001b[0m");
+            }
+            public static String colorize(String text,int c1,int c2){
+              return ("\u001b[" + c1 + ";" + c2 + "m"+text+"\u001b[0m");
+            }
+            public static String colorize(String text,int c1,int c2,int c3){
+              return ("\u001b[" + c1 + ";" + c2 + ";" + c3 + "m"+text+"\u001b[0m");
+            }
           }
