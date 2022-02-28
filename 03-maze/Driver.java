abb85f300c8bb24af198fa5d9874fd4c5e4fd65c
Shafiul Haque <shaque30@stuy.edu>
2022-02-28 10:46:56 -0500

    imported file from website

---
+++ 03-maze/Driver.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Driver{
+            public static void main(String[]args){
+
+              //files do not require an extension like .txt or .dat
+              String filename = "maze1";
+
+              try{
+                Maze f;
+                f = new Maze(filename);
+                f.setAnimate(true);
+                System.out.println(f.solve()+" steps");
+                System.out.println(f);
+              }catch(FileNotFoundException e){
+                System.out.println("Invalid filename: "+filename);
+              }
+            }
+          }
