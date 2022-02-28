704407e122de3323bde2b105b39109f884c3a747
Shafiul Haque <shaque30@stuy.edu>
2022-02-28 10:45:58 -0500

    made some modifications over the break

--- Classwork/MRTester.java
+++ Classwork/MRTester.java
@@ -3,15 +3,11 @@ import java.util.*;
 
 public class MRTester{
   public static void main (String args[]){
-    char[][]nums;
     try{
-      nums = MazeReader.getVals("Maze1.txt");
-      for (int i = 0; i < nums.length; i++){
-        for (int j = 0; j < nums[i].length; j++){
-          System.out.print(nums[i][j]);
-        }
-        System.out.println();
-      }
+      MazeReader joe = new MazeReader("Maze1.txt");
+      System.out.println(joe);
+      int[] joe2 = joe.findStart();
+      System.out.println(joe2[0] + " " + joe2[1]);
     }catch(FileNotFoundException e){
       System.out.println("FileNotFoundException is occurred");
     }
--- Classwork/MazeReader.java
+++ Classwork/MazeReader.java
@@ -2,7 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class MazeReader {
-  public static char[][] getVals(String filename) throws FileNotFoundException{
+  //fields
+  private char[][] maze;
+  private boolean animated;
+  private int delay;
+
+  //constructor
+  public MazeReader(String filename) throws FileNotFoundException{
+    this.maze = getVals(filename);
+    this.delay = 1000;
+    this.animated = false;
+  }
+
+  //mutator methods
+  public void setAnimate(boolean newValue){
+    this.animated = newValue;
+  }
+
+  public void setDelay(int newValue){
+    if (newValue < 0) this.delay = 0;
+    this.delay = newValue;
+  }
+
+  //reading methods
+  public char[][] getVals(String filename) throws FileNotFoundException{
     ArrayList<char[]> joe = new ArrayList<char[]>();
     int counter = 0;
     File text = new File(filename);
@@ -20,5 +43,44 @@ public class MazeReader {
     }
     return maze;
   }
+
+  public String toString(){
+    String mr = "";
+    for (int i = 0; i < maze.length; i++){
+      for (int j = 0; j < maze[i].length; j++){
+        System.out.print(maze[i][j]);
+      }
+      System.out.println();
+    }
+    return mr;
+  }
+
+  //solvers
+  public int[] findStart(){
+    int[] bruh = new int[2];
+    for (int i = 0; i < maze.length; i++){
+      for (int j = 0; j < maze[i].length; j++){
+        if (maze[i][j] == 'S'){
+          bruh[0] = i;
+          bruh[1] = j;
+        }
+      }
+    }
+    return bruh;
+  }
+
+  // private int[] canGo(){
+  //
+  // }
+  //
+  // public int[][] moveWhere(){
+  //   int[] joe = findStart();
+  //   if (joe[0] > 1){
+  //     if ()
+  //   }
+  // }
+
+
+
 
 }
