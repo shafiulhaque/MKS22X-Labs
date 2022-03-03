de6203608fc2328dd93497e2f18bb959bea33018
Shafiul Haque <shaque30@stuy.edu>
2022-03-03 10:28:19 -0500

    made some args modifications to the driver

--- 03-maze/Driver.java
+++ 03-maze/Driver.java
@@ -4,12 +4,13 @@ public class Driver{
             public static void main(String[]args){
 
               //files do not require an extension like .txt or .dat
-              String filename = "maze1";
+              String filename = args[0];
+              boolean thing = Boolean.parseBoolean(args[1]);
 
               try{
                 Maze f;
                 f = new Maze(filename);
-                f.setAnimate(true);
+                f.setAnimate(thing);
                 System.out.println(f);
                 System.out.println(f.solve()+" steps");
                 System.out.println(f);
