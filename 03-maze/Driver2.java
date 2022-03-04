ac74062cb7abab2b9d9631b59483459a00c0b6dd
Shafiul Haque <shaque30@stuy.edu>
2022-03-04 10:47:39 -0500

    finished general maze generator, and tested it out in a new driver

--- 03-maze/Driver2.java
+++ 03-maze/Driver2.java
@@ -1,11 +1,12 @@
 public class Driver2{
   public static void main(String args[]){
-    char[][] joe = new char[6][7];
+    char[][] joe = new char[8][10];
     for (int i = 0; i < joe.length; i++){
       for (int j = 0; j < joe[i].length; j++){
         joe[i][j] = '#';
       }
     }
     MazeGenerator.generate(joe, 2, 2);
+    System.out.println(MazeGenerator.toString(joe));
   }
 }
--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,24 +1,26 @@
 public class MazeGenerator{
   public static boolean canCarve(char[][]maze, int row, int col){
-    if (row == 0 || row == maze.length-1 || col == 0 || col == maze[0].length-1) return false;
+    if (row == 0 || row >= maze.length-1 || col == 0 || col >= maze[0].length-1) return false;
     int counter = 0;
-    if (maze[row][col-1] == '#') counter++;
-    if (maze[row][col+1] == '#') counter++;
-    if (maze[row-1][col] == '#') counter++;
-    if (maze[row+1][col] == '#') counter++;
+    if (maze[row][col-1] == ' ') counter++;
+    if (maze[row][col+1] == ' ') counter++;
+    if (maze[row-1][col] == ' ') counter++;
+    if (maze[row+1][col] == ' ') counter++;
     return (counter < 2);
   }
   public static void generate(char[][]maze, int startrow, int startcol){
+    if (maze[startrow][startcol] != ' '){
     if (canCarve(maze, startrow, startcol)){
       maze[startrow][startcol] = ' ';
+      generate(maze, startrow+1, startcol);
+      generate(maze, startrow-1, startcol);
+      generate(maze, startrow, startcol+1);
+      generate(maze, startrow, startcol-1);
     }
-    generate(maze, startrow+1, startcol);
-    generate(maze, startrow-1, startcol);
-    generate(maze, startrow, startcol+1);
-    generate(maze, startrow, startcol-1);
-
   }
-  public String toString(char[][]maze){
+  }
+
+  public static String toString(char[][]maze){
       String mr = "";
       for (int i = 0; i < maze.length; i++){
         for (int j = 0; j < maze[i].length; j++){
