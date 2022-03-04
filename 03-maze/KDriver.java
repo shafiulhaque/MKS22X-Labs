294a1461d8f27fa2b9ab6e48cc1de69a4ad28516
Shafiul Haque <shaque30@stuy.edu>
2022-03-04 13:58:19 -0500

    added the KDriver and added the necessary components, also finished MazeGenerator

---
+++ 03-maze/KDriver.java
@@ -0,0 +1,38 @@
+import java.io.*;
+  public class KDriver{
+      public static void main(String[]args){
+          if(args.length == 4){
+            int rows = Integer.parseInt(args[0]);
+            int cols = Integer.parseInt(args[1]);
+            int startrow = Integer.parseInt(args[2]);
+            int startcol = Integer.parseInt(args[3]);
+            char[][]maze = new char[rows][cols];
+            for (int i = 0; i < maze.length; i++){
+              for (int j = 0; j < maze[i].length; j++){
+                maze[i][j] = '#';
+              }
+            }
+            MazeGenerator.generate(maze,startrow,startcol);
+            MazeGenerator.toString(maze);
+            System.out.println("MAZE!");//You need a way to show the maze without the array brackets etc.
+
+          }else if(args.length == 1  || args.length == 2){
+              try{
+                  Maze m = new Maze(args[0]);
+                  if(args.length == 2){ //any 2nd parameter turns on animate.
+                      m.setAnimate(true);
+                  }
+                  System.out.println(m.solve());
+                  System.out.println(m);
+              }catch(FileNotFoundException e){
+                  System.out.println(e);
+              }
+          }else{
+              System.out.println("Usage:");
+              System.out.println("To solve:    java KDriver FILE_NAME");
+              System.out.println("To animate:  java KDriver FILE_NAME animated");
+              System.out.println("To Generate: java KDriver rows cols startRow startCol");
+          }
+
+      }
+  }
--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -12,37 +12,51 @@ public class MazeGenerator{
     return (counter < 2);
   }
   public static void generate(char[][]maze, int startrow, int startcol){
+    generate(maze, startrow, startcol, true);
+  }
+
+  public static void generate(char[][]maze, int startrow, int startcol, boolean first){
     if (maze[startrow][startcol] != ' '){
     if (canCarve(maze, startrow, startcol)){
       maze[startrow][startcol] = ' ';
       Random randgen = new Random();
       int joe = randgen.nextInt(4);
       if (joe == 0){
-        generate(maze, startrow+1, startcol);
-        generate(maze, startrow-1, startcol);
-        generate(maze, startrow, startcol+1);
-        generate(maze, startrow, startcol-1);
+        generate(maze, startrow+1, startcol, false);
+        generate(maze, startrow-1, startcol, false);
+        generate(maze, startrow, startcol+1, false);
+        generate(maze, startrow, startcol-1, false);
       }
       if (joe == 1){
-        generate(maze, startrow-1, startcol);
-        generate(maze, startrow, startcol+1);
-        generate(maze, startrow, startcol-1);
-        generate(maze, startrow+1, startcol);
+        generate(maze, startrow-1, startcol, false);
+        generate(maze, startrow, startcol+1, false);
+        generate(maze, startrow, startcol-1, false);
+        generate(maze, startrow+1, startcol, false);
       }
       if (joe == 2){
-        generate(maze, startrow, startcol+1);
-        generate(maze, startrow, startcol-1);
-        generate(maze, startrow+1, startcol);
-        generate(maze, startrow-1, startcol);
+        generate(maze, startrow, startcol+1, false);
+        generate(maze, startrow, startcol-1, false);
+        generate(maze, startrow+1, startcol, false);
+        generate(maze, startrow-1, startcol, false);
       }
       if (joe == 3){
-        generate(maze, startrow, startcol+1);
-        generate(maze, startrow, startcol-1);
-        generate(maze, startrow+1, startcol);
-        generate(maze, startrow-1, startcol);
+        generate(maze, startrow, startcol+1, false);
+        generate(maze, startrow, startcol-1, false);
+        generate(maze, startrow+1, startcol, false);
+        generate(maze, startrow-1, startcol, false);
       }
     }
   }
+  if (first){
+    maze[startrow][startcol] = 'S';
+    for (int j = maze[0].length-2; j > 0; j--){
+    if (maze[maze.length-2][j] == ' '){
+      maze[maze.length-2][j] = 'E';
+      break;
+    }
+  }
+}
+
   }
 
   public static String toString(char[][]maze){
