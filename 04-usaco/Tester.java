2181c05b5658e46b77050a2ef84383b859bf12cd
Shafiul Haque <shaque30@stuy.edu>
2022-03-08 20:44:09 -0500

    technical mistake w tester, put main functions by accident

--- 04-usaco/Tester.java
+++ 04-usaco/Tester.java
@@ -11,28 +11,3 @@ public class Tester{
     }
   }
 }
-
-
-  public static int[][] elevation(int[][]grid, int elev){
-    for(int i = 0; i < grid.length; i++){
-      for (int j = 0; j < grid[i].length; j++){
-        if (grid[i][j] > elev){
-          grid[i][j] = 0;
-        } else {
-          grid[i][j] = elev - grid[i][j];
-        }
-      }
-    }
-    return grid;
-  }
-
-  public static long calc(int[][] grid){
-    long counter = 0;
-    for(int i = 0; i < grid.length; i++){
-      for (int j = 0; j < grid[i].length; j++){
-        counter += grid[i][j];
-      }
-    }
-    counter = counter*72*72;
-    return counter;
-  }
