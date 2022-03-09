c8059ffba0c2e26805422c400857c3b50e3d43d3
Shafiul Haque <shaque30@stuy.edu>
2022-03-09 10:03:58 -0500

    made a canmove function

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -37,16 +37,48 @@ public class Silver {
     grid[x1-1][y1-1] = 'C';
     grid[x2-1][y2-1] = 'E';
     toString(grid);
-
     return 0;
   }
-  public static void toString(char[][] joe){
-    for (int i = 0; i < joe.length; i++){
-      for (int j = 0; j < joe[i].length; j++){
-        System.out.print(joe[i][j]);
+
+  public static void toString(char[][] grid){
+    for (int i = 0; i < grid.length; i++){
+      for (int j = 0; j < grid[i].length; j++){
+        System.out.print(grid[i][j]);
       }
       System.out.println();
     }
+  }
+
+  public static boolean canMove(char[][] grid, int x1, int y1, char joe){
+    if (joe == 'U'){
+      if (x1 > 0 && grid[x1-1][y1] != '*'){
+        grid[x1][y1] = '.';
+        grid[x1-1][y1] = 'C';
+        return true;
+      }
+    }
+    if (joe == 'D'){
+      if (x1 < grid.length && grid[x1+1][y1] != '*'){
+        grid[x1][y1] = '.';
+        grid[x1+1][y1] = 'C';
+        return true;
+      }
+    }
+    if (joe == 'L'){
+      if (y1 > 0 && grid[x1][y1-1] != '*'){
+        grid[x1][y1] = '.';
+        grid[x1][y1-1] = 'C';
+        return true;
+      }
+    }
+    if (joe == 'R'){
+      if (y1 < grid[x1].length && grid[x1][y1+1] != '*'){
+        grid[x1][y1] = '.';
+        grid[x1][y1+2] = 'C';
+        return true;
+      }
+    }
+    return false;
   }
 
   public static long solver(char[][] joe, int row, int col){
@@ -56,6 +88,7 @@ public class Silver {
     } else if (joe[row][col] == '*' || joe[row][col] == '-' || joe[row][col] == '.'){
       return -1;
     }
+    return counter;
     }
 
 }
