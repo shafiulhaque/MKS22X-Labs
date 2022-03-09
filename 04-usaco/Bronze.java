4845b750bbcb29e5858c2d019b9e5508afdee48b
Shafiul Haque <shaque30@stuy.edu>
2022-03-08 20:42:20 -0500

    finished w two other functions

--- 04-usaco/Bronze.java
+++ 04-usaco/Bronze.java
@@ -76,4 +76,28 @@ public class Bronze{
   }
   return grid;
   }
+
+  public static int[][] elevation(int[][]grid, int elev){
+    for(int i = 0; i < grid.length; i++){
+      for (int j = 0; j < grid[i].length; j++){
+        if (grid[i][j] > elev){
+          grid[i][j] = 0;
+        } else {
+          grid[i][j] = elev - grid[i][j];
+        }
+      }
+    }
+    return grid;
+  }
+
+  public static long calc(int[][] grid){
+    long counter = 0;
+    for(int i = 0; i < grid.length; i++){
+      for (int j = 0; j < grid[i].length; j++){
+        counter += grid[i][j];
+      }
+    }
+    counter = counter*72*72;
+    return counter;
+  }
 }
