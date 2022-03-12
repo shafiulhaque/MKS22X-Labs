da3206da876b67c4dc12903ffdf6d3f024368705
Shafiul Haque <shaque30@stuy.edu>
2022-03-12 15:41:33 -0500

    dont need trycatch anymore

--- 04-usaco/Tester.java
+++ 04-usaco/Tester.java
@@ -3,14 +3,9 @@ import java.io.*;
 
 public class Tester{
   public static void main(String args[]){
-    try{
-      long ans = Bronze.solve(args[0]);
-      System.out.println(ans);
-      long ans2 = Silver.solve(args[1]);
-      System.out.println(ans2);
-
-    }catch(FileNotFoundException e){
-                //error handling code
-    }
+    long ans = Bronze.solve(args[0]);
+    System.out.println(ans);
+    long ans2 = Silver.solve(args[1]);
+    System.out.println(ans2);
   }
 }
