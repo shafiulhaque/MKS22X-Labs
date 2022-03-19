ee935198bdb7f763275e15274b3e13f790d844c1
Shafiul Haque <shaque30@stuy.edu>
2022-03-19 13:20:30 -0400

    trying to find convergence value

--- 05-quick/insertquick.java
+++ 05-quick/insertquick.java
@@ -1,26 +1,34 @@
 public class insertquick{
   public static void main(String args[]){
-    int[][] bruh = new int[2][1000];
+    int joe = Integer.parseInt(args[0]);
+
+    int[][] bro = new int[2][joe];
+    int[] bro1 = new int[joe];
+    for(int j = 0; j < joe; j++){
+      bro1[j] =(int)(Math.random()*100000);
+    }
+
     for(int i = 0; i < 2; i++){
-      for (int j = 0; j < 1000; j++){
-        bruh[i][j] =(int)(Math.random()*100000);
+      for (int j = 0; j < joe; j++){
+        bro[i][j] = bro1[j];
       }
     }
-    long start = System.currentTimeMillis();
+    
+    long start = System.nanoTime();
     for(int i = 0; i < 1; i++){
-      Quick.quicksort(bruh[i]);
+      Quick.quicksort(bro[i]);
     }
-    long end = System.currentTimeMillis();
-    long bruh2 = end - start;
-    System.out.println("QUICK SORT: " + bruh2);
+    long end = System.nanoTime();
+    long bruh = end - start;
+    System.out.println("QUICK SORT: " + end + ", " + start + ", " + bruh);
 
-    long start2 = System.currentTimeMillis();
+    long start2 = System.nanoTime();
     for(int i = 0; i < 1; i++){
-      Quick.insertionSort(bruh[i+1]);
+      Quick.insertionSort(bro[i+1]);
     }
-    long end2 = System.currentTimeMillis();
-    long bruh3 = end2 - start2;
-    System.out.println("INSERTION SORT: " + bruh3);
+    long end2 = System.nanoTime();
+    long bruh2 = end2 - start2;
+    System.out.println("INSERTION SORT: " + end2 + ", " + start2 + ", " + bruh2);
 
   }
 }
