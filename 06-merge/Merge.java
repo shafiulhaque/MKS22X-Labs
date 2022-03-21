91093baba83738928e595df211dd34f08b8af64d
Shafiul Haque <shaque30@stuy.edu>
2022-03-21 10:43:55 -0400

    new stuff for the merge sort

---
+++ 06-merge/Merge.java
@@ -0,0 +1,20 @@
+public class Merge{
+  int [] merge(int [] left, int[] right){
+          //return a new array that is the merged version of left and right
+        }
+
+        void mergesort(data){
+          int [] temp  =  mergeSortH(data);
+          copy temp into data
+        }
+
+        int[] mergesortH(data){
+          if more than 1 element{
+            left = copy half of data
+            right = copy other half of data
+            mergesort(left)
+            mergesort(right)
+            return merge(left,right)
+          }
+        }
+}
