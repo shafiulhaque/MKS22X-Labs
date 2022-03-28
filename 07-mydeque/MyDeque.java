c0181f73f0f937a93b6c5b53c0d7b03609d4bf56
Shafiul Haque <shaque30@stuy.edu>
2022-03-28 14:02:15 -0400

    finished code, added all the methods

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -1,4 +1,5 @@
 public class MyDeque<E>{
+      //fields
       private E[] data;
       private int size;
       private int start, end;
@@ -9,23 +10,28 @@ public class MyDeque<E>{
         E[] d = (E[])new Object[10];
         data = d;
         size = 0;
-        start = data.length/2;
-        end = start + 1;
+        start = (data.length/2);
+        end = start-1;
       }
-
       public MyDeque(int initialCapacity){
         @SuppressWarnings("unchecked")
         E[] d = (E[])new Object[initialCapacity];
         data = d;
         size = 0;
-        start = data.length/2;
-        end = start + 1;
+        start = (data.length/2);
+        end = start - 1;
       }
 
+      //accessor methods
       public int size(){
         return size;
       }
-
+      public E getFirst(){
+        return data[start];
+      }
+      public E getLast(){
+        return data[end];
+      }
       public String toString(){
         String joe = "[";
         for(int i = 0; i < data.length-1; i++){
@@ -35,11 +41,15 @@ public class MyDeque<E>{
         return joe;
       }
 
+      //adding
       public void addFirst(E element){
         if (size == 0){
+          start--;
           data[start] = element;
+          size++;
         } else if (size == data.length){
           resize();
+          addFirst(element);
         } else if (start == 0){
           start = data.length;
           addFirst(element);
@@ -49,13 +59,15 @@ public class MyDeque<E>{
           size++;
         }
       }
-
       public void addLast(E element){
         if (size == 0){
+          end++;
           data[end] = element;
+          size++;
         } else if (size == data.length){
           resize();
-        } else if (end == data.length){
+          addLast(element);
+        } else if (end+1 == data.length){
           end = -1;
           addLast(element);
         } else {
@@ -65,10 +77,44 @@ public class MyDeque<E>{
         }
       }
 
+      //resize
       private void resize(){
-        
+        @SuppressWarnings("unchecked")
+        E[] L = (E[])new Object[data.length*2];
+        if (start == 0 && end == data.length - 1){
+          int joe = data.length/2;
+          for (int i = 0; i < joe; i++){
+            L[i] = null;
+          }
+          for (int i = 0; i < data.length; i++){
+            L[i+joe] = data[i];
+          }
+          for (int i = data.length + joe; i < data.length*2; i++){
+            L[i] = null;
+          }
+          start += joe;
+          end += joe;
+        } else if(start > end){
+          for(int i = 0; i <= end; i++){
+            L[i] = data[i];
+          }
+          for(int i = start; i < data.length; i++){
+            L[i + data.length] = data[i];
+          }
+          start += data.length;
+        } else {
+          for(int i = 0; i <= start; i++){
+            L[i] = data[i];
+          }
+          for(int i = end; i < data.length; i++){
+            L[i + data.length] = data[i];
+          }
+          end += data.length;
+        }
+        data = L;
       }
 
+      //remove
       public E removeFirst(){
         E joe = data[start];
         data[start] = null;
@@ -80,7 +126,6 @@ public class MyDeque<E>{
         size--;
         return joe;
       }
-
       public E removeLast(){
         E joe = data[end];
         data[end] = null;
@@ -91,12 +136,5 @@ public class MyDeque<E>{
         }
         size--;
         return joe;
-      }
-
-      public E getFirst(){
-        return data[start];
-      }
-      public E getLast(){
-        return data[end];
       }
     }
