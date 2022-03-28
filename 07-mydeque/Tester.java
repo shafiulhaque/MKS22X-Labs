f9dd112e6a7e2787570b95cac267c14b9e9c6525
Shafiul Haque <shaque30@stuy.edu>
2022-03-28 14:02:28 -0400

    added test cases

--- 07-mydeque/Tester.java
+++ 07-mydeque/Tester.java
@@ -1,6 +1,16 @@
 public class Tester{
-  public static void main(String args){
+  public static void main(String[] args){
     MyDeque<Integer> joe = new MyDeque<Integer>(10);
-    joe.toString();
+    System.out.println(joe.toString());
+    joe.addFirst(21);
+    joe.addFirst(42);
+    joe.addFirst(652);
+    joe.addFirst(8);
+    joe.addFirst(62);
+    joe.addFirst(97);
+    joe.addLast(22);
+    joe.removeLast();
+    joe.addLast(22);
+    System.out.println(joe.toString());
   }
 }
