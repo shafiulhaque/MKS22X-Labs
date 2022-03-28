74f3c72ef037c993a92937d74aad69dfd7e494d7
Shafiul Haque <shaque30@stuy.edu>
2022-03-28 10:25:44 -0400

    made new mydeque file

---
+++ 07-mydeque/MyDeque.java
@@ -0,0 +1,16 @@
+public class MyDeque<E>{
+      private E[] data;
+      private int size;
+      private int start, end;
+
+      public MyDeque(){  }
+      public MyDeque(int initialCapacity){  }
+      public int size(){ }
+      public String toString(){ }
+      public void addFirst(E element){ }
+      public void addLast(E element){ }
+      public E removeFirst(){ }
+      public E removeLast(){ }
+      public E getFirst(){ }
+      public E getLast(){ }
+    }
