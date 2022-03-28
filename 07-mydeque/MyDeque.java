eabada21d1c536937e1e45d66856783c3127be32
Shafiul Haque <shaque30@stuy.edu>
2022-03-28 10:32:13 -0400

    started the construction and to string method

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -3,10 +3,36 @@ public class MyDeque<E>{
       private int size;
       private int start, end;
 
-      public MyDeque(){  }
-      public MyDeque(int initialCapacity){  }
-      public int size(){ }
-      public String toString(){ }
+      //constructors
+      public MyDeque(){
+        @SuppressWarnings("unchecked")
+        E[] d = (E[])new Object[10];
+        data = d;
+        size = 0;
+        start = data.length/2;
+        end = start + 1;
+      }
+
+      public MyDeque(int initialCapacity){
+        @SuppressWarnings("unchecked")
+        E[] d = (E[])new Object[initialCapacity];
+        data = d;
+        size = 0;
+      }
+
+      public int size(){
+        return size;
+      }
+
+      public String toString(){
+        String joe = "[";
+        for(int i = 0; i < data.length-1; i++){
+          joe += data[i] + ", ";
+        }
+        joe += data[data.length-1] + "]";
+        return joe;
+      }
+      
       public void addFirst(E element){ }
       public void addLast(E element){ }
       public E removeFirst(){ }
