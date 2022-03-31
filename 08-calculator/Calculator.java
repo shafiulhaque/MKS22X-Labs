538eaeb3e63576a443269ae18da6aa347f1ed194
Shafiul Haque <shaque30@stuy.edu>
2022-03-31 10:28:35 -0400

    added placeholder code

---
+++ 08-calculator/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator{
+      /*Evaluate a postfix expression stored in s.
+      *Assume valid postfix notation, of ints doubles and operators separated by spaces.
+      *Valid operators are + - / * and % (remainder not modulo)
+      *All results are doubles even if the operands are both int.
+      *@throws IllegalArgumentException when there are too many or too few operands.
+      *        Use the string parameter of your exception to indicate what happened.
+      *        Either "too many operands" or "too few operands for operation _" replace _ with +,- etc.
+      */
+      public static double eval(String s){}
+    }
+  
