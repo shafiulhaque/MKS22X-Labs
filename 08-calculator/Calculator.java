b5c7ffc2549c2ab3a61a572e8b1823657dcf27cf
Shafiul Haque <shaque30@stuy.edu>
2022-03-31 10:40:28 -0400

    started the code

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class Calculator{
       /*Evaluate a postfix expression stored in s.
       *Assume valid postfix notation, of ints doubles and operators separated by spaces.
@@ -7,6 +10,18 @@ public class Calculator{
       *        Use the string parameter of your exception to indicate what happened.
       *        Either "too many operands" or "too few operands for operation _" replace _ with +,- etc.
       */
-      public static double eval(String s){}
+      public static double eval(String s){
+        Deque<Double> bruh = new LinkedList<Double>();
+        while(s.contains(" ") && s.length() > 0){
+          String joe = s.substring(0, s.indexOf(" "));
+          if(joe != "+" && joe != "-" && joe != "/" && joe != "*" && joe != "%"){
+            bruh.addFirst(Double.parseDouble(joe));
+          }
+          s = s.substring(s.indexOf(" "));
+        }
+        return bruh.size();
+      }
+      public static void main(String[] args){
+        System.out.println(eval("11 3 - 4 + 2.5 *"));
+      }
     }
-  
